1ffd62c274ef2ed45bb52feb50f52da1cb1231eb
Vikram <13096355+vikramadhav@users.noreply.github.com>
2026-06-16 06:53:39 +0000

    Further Improvisation

--- sw.js
+++ sw.js
@@ -13,19 +13,14 @@ const PRECACHE_URLS = [
   "/assets/vendor/bootstrap/css/bootstrap.min.css",
   "/assets/vendor/bootstrap/js/bootstrap.bundle.min.js",
   "/assets/vendor/jquery/jquery.min.js",
-  "/assets/vendor/jquery.easing/jquery.easing.min.js",
   "/assets/vendor/php-email-form/validate.js",
   "/assets/vendor/waypoints/jquery.waypoints.min.js",
   "/assets/vendor/counterup/counterup.min.js",
-  "/assets/vendor/owl.carousel/owl.carousel.min.js",
-  "/assets/vendor/owl.carousel/assets/owl.carousel.min.css",
-  "/assets/vendor/isotope-layout/isotope.pkgd.min.js",
-  "/assets/vendor/venobox/venobox.css",
-  "/assets/vendor/venobox/venobox.min.js",
   "/assets/vendor/remixicon/remixicon.css",
   "/assets/vendor/icofont/icofont.min.css",
   "/assets/vendor/boxicons/css/boxicons.min.css",
-  "/assets/img/programmer.svg"
+  "/assets/img/programmer.svg",
+  "/assets/img/bg-min-mobile.jpg"
 ];
 
 self.addEventListener("install", (event) => {
